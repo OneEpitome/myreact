fc23f3c59daf2a07ea247725eb4429f110bd2449
OneEpitome <rkddlfah0319@gmail.com>
2022-12-24 11:07:38 +0900

    await-async 적용2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React from "react";
 function App() {
   const [loading, setLoading] = React.useState(true);
   const [movies, setMovies] = React.useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  React.useEffect(() => {
+    getMovies();
+  }, []);
 
   // React.useEffect(() => {
   //   fetch(
