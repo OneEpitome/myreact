f50534acf22de33c0cd184cce1f0593012a3338d
OneEpitome <rkddlfah0319@gmail.com>
2022-12-24 11:39:33 +0900

    Movie App part two clear

--- src/App.js
+++ src/App.js
@@ -1,57 +1,8 @@
 import React from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = React.useState(true);
-  const [movies, setMovies] = React.useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&&sort_by=year"
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  React.useEffect(() => {
-    getMovies();
-  }, []);
-
-  // React.useEffect(() => {
-  //   fetch(
-  //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&&sort_by=year"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setLoading(false);
-  //       setMovies(json.data.movies);
-  //     });
-  // }, []);
-
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading now ... </h1>
-      ) : (
-        <div>
-          {movies.map((movie) => {
-            return (
-              <div key={movie.id}>
-                <img alt="cover_image" src={movie.medium_cover_image} />
-                <h2>{movie.title}</h2>
-                <p>{movie.summary}</p>
-                <ol>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </ol>
-              </div>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+import propTypes from "prop-types";
+
+function Movie({ medium_cover_image, title, summary, genres }) {
+  return (
+    <div>
+      <img alt="cover_image" src={medium_cover_image} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  medium_cover_image: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string),
+};
+
+export default Movie;
